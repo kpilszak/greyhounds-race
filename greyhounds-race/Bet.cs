cecb65496e6bf7b07cef7533c2bea9dcc9ed008b
kpilszak <klaudia.pilszak@gmail.com>
2018-11-30 14:54:02 +0100

    Draft of Bet class

--- greyhounds-race/Bet.cs
+++ greyhounds-race/Bet.cs
@@ -2,5 +2,17 @@
 
 public class Bet
 {
-	
+    public int Amount;
+    public int Dog;
+    public Player Bettor;
+
+    public string GetDescription()
+    {
+
+    }
+
+    public int PayOut(int Winner)
+    {
+
+    }
 }
