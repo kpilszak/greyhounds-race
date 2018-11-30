06720aa0bf31d1edb8bd93d4baacd35ceee956cf
kpilszak <klaudia.pilszak@gmail.com>
2018-11-30 21:14:48 +0100

    Add UpdateLabels()

--- greyhounds-race/Player.cs
+++ greyhounds-race/Player.cs
@@ -5,12 +5,13 @@ public class Player
     public string Name;
     public Bet MyBet;
     public int Cash;
-    public RadioButton MyRadioButton;
-    public Label MyLabel;
+    public RadioButton PlayerButton;
+    public Label BetLabel;
 
     public void UpdateLabels()
     {
-
+        BetLabel.Text = MyBet.GetDescription();
+        PlayerButton.Text = Name + " has " + Cash + "$";
     }
 
     public void ClearBet()
