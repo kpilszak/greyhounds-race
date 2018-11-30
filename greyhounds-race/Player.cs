6a05a117047193a09e2513877bf5b80c60fbe908
kpilszak <klaudia.pilszak@gmail.com>
2018-11-30 14:52:33 +0100

    Draft of Player class

--- greyhounds-race/Player.cs
+++ greyhounds-race/Player.cs
@@ -2,5 +2,29 @@
 
 public class Player
 {
-	
+    public string Name;
+    public Bet MyBet;
+    public int Cash;
+    public RadioButton MyRadioButton;
+    public Label MyLabel;
+
+    public void UpdateLabels()
+    {
+
+    }
+
+    public void ClearBet()
+    {
+
+    }
+
+    public void PlaceBet(int Amount, int DogToWin)
+    {
+
+    }
+
+    public void Collect(int Winner)
+    {
+
+    }
 }
