eecbaef723ae3bce5998be55466a69696cde25b9
kpilszak <klaudia.pilszak@gmail.com>
2018-11-30 20:00:26 +0100

    Draw form

--- greyhounds-race/Form1.Designer.cs
+++ greyhounds-race/Form1.Designer.cs
@@ -28,33 +28,284 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.raceTrackPictureBox = new System.Windows.Forms.PictureBox();
+            this.greyhoundPictureBox1 = new System.Windows.Forms.PictureBox();
+            this.greyhoundPictureBox2 = new System.Windows.Forms.PictureBox();
+            this.greyhoundPictureBox3 = new System.Windows.Forms.PictureBox();
+            this.greyhoundPictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.minimumBetLabel = new System.Windows.Forms.Label();
+            this.playerRadioButton1 = new System.Windows.Forms.RadioButton();
+            this.playerRadioButton2 = new System.Windows.Forms.RadioButton();
+            this.playerRadioButton3 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.playerTextBox1 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.Label();
+            this.betsButton = new System.Windows.Forms.Button();
+            this.amountButton = new System.Windows.Forms.DomainUpDown();
+            this.betLabel = new System.Windows.Forms.Label();
+            this.grehoundNumberButton = new System.Windows.Forms.DomainUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.raceTrackPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greyhoundPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greyhoundPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greyhoundPictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greyhoundPictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // startButton
             // 
-            this.button1.Location = new System.Drawing.Point(718, 393);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(590, 264);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(237, 300);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start!";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // raceTrackPictureBox
+            // 
+            this.raceTrackPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("raceTrackPictureBox.Image")));
+            this.raceTrackPictureBox.Location = new System.Drawing.Point(13, 13);
+            this.raceTrackPictureBox.Name = "raceTrackPictureBox";
+            this.raceTrackPictureBox.Size = new System.Drawing.Size(814, 230);
+            this.raceTrackPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.raceTrackPictureBox.TabIndex = 1;
+            this.raceTrackPictureBox.TabStop = false;
+            // 
+            // greyhoundPictureBox1
+            // 
+            this.greyhoundPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("greyhoundPictureBox1.Image")));
+            this.greyhoundPictureBox1.Location = new System.Drawing.Point(25, 23);
+            this.greyhoundPictureBox1.Name = "greyhoundPictureBox1";
+            this.greyhoundPictureBox1.Size = new System.Drawing.Size(105, 28);
+            this.greyhoundPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.greyhoundPictureBox1.TabIndex = 2;
+            this.greyhoundPictureBox1.TabStop = false;
+            // 
+            // greyhoundPictureBox2
+            // 
+            this.greyhoundPictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("greyhoundPictureBox2.Image")));
+            this.greyhoundPictureBox2.Location = new System.Drawing.Point(25, 76);
+            this.greyhoundPictureBox2.Name = "greyhoundPictureBox2";
+            this.greyhoundPictureBox2.Size = new System.Drawing.Size(105, 28);
+            this.greyhoundPictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.greyhoundPictureBox2.TabIndex = 3;
+            this.greyhoundPictureBox2.TabStop = false;
+            // 
+            // greyhoundPictureBox3
+            // 
+            this.greyhoundPictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("greyhoundPictureBox3.Image")));
+            this.greyhoundPictureBox3.Location = new System.Drawing.Point(25, 140);
+            this.greyhoundPictureBox3.Name = "greyhoundPictureBox3";
+            this.greyhoundPictureBox3.Size = new System.Drawing.Size(105, 28);
+            this.greyhoundPictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.greyhoundPictureBox3.TabIndex = 4;
+            this.greyhoundPictureBox3.TabStop = false;
+            // 
+            // greyhoundPictureBox4
+            // 
+            this.greyhoundPictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("greyhoundPictureBox4.Image")));
+            this.greyhoundPictureBox4.Location = new System.Drawing.Point(25, 199);
+            this.greyhoundPictureBox4.Name = "greyhoundPictureBox4";
+            this.greyhoundPictureBox4.Size = new System.Drawing.Size(105, 28);
+            this.greyhoundPictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.greyhoundPictureBox4.TabIndex = 5;
+            this.greyhoundPictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 30);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Betting House";
+            // 
+            // minimumBetLabel
+            // 
+            this.minimumBetLabel.AutoSize = true;
+            this.minimumBetLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimumBetLabel.Location = new System.Drawing.Point(30, 312);
+            this.minimumBetLabel.Name = "minimumBetLabel";
+            this.minimumBetLabel.Size = new System.Drawing.Size(171, 25);
+            this.minimumBetLabel.TabIndex = 7;
+            this.minimumBetLabel.Text = "Minimum bet: 5$";
+            // 
+            // playerRadioButton1
+            // 
+            this.playerRadioButton1.AutoSize = true;
+            this.playerRadioButton1.Location = new System.Drawing.Point(35, 364);
+            this.playerRadioButton1.Name = "playerRadioButton1";
+            this.playerRadioButton1.Size = new System.Drawing.Size(175, 24);
+            this.playerRadioButton1.TabIndex = 8;
+            this.playerRadioButton1.TabStop = true;
+            this.playerRadioButton1.Text = "playerRadioButton1";
+            this.playerRadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // playerRadioButton2
+            // 
+            this.playerRadioButton2.AutoSize = true;
+            this.playerRadioButton2.Location = new System.Drawing.Point(35, 421);
+            this.playerRadioButton2.Name = "playerRadioButton2";
+            this.playerRadioButton2.Size = new System.Drawing.Size(175, 24);
+            this.playerRadioButton2.TabIndex = 9;
+            this.playerRadioButton2.TabStop = true;
+            this.playerRadioButton2.Text = "playerRadioButton2";
+            this.playerRadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // playerRadioButton3
+            // 
+            this.playerRadioButton3.AutoSize = true;
+            this.playerRadioButton3.Location = new System.Drawing.Point(35, 483);
+            this.playerRadioButton3.Name = "playerRadioButton3";
+            this.playerRadioButton3.Size = new System.Drawing.Size(175, 24);
+            this.playerRadioButton3.TabIndex = 10;
+            this.playerRadioButton3.TabStop = true;
+            this.playerRadioButton3.Text = "playerRadioButton3";
+            this.playerRadioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(298, 312);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Bets";
+            // 
+            // playerTextBox1
+            // 
+            this.playerTextBox1.Location = new System.Drawing.Point(303, 364);
+            this.playerTextBox1.Name = "playerTextBox1";
+            this.playerTextBox1.Size = new System.Drawing.Size(252, 26);
+            this.playerTextBox1.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(303, 421);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 26);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(303, 483);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 26);
+            this.textBox2.TabIndex = 14;
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(31, 538);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(49, 20);
+            this.name.TabIndex = 15;
+            this.name.Text = "name";
+            // 
+            // betsButton
+            // 
+            this.betsButton.Location = new System.Drawing.Point(126, 533);
+            this.betsButton.Name = "betsButton";
+            this.betsButton.Size = new System.Drawing.Size(75, 31);
+            this.betsButton.TabIndex = 16;
+            this.betsButton.Text = "bets";
+            this.betsButton.UseVisualStyleBackColor = true;
+            // 
+            // amountButton
+            // 
+            this.amountButton.Location = new System.Drawing.Point(219, 536);
+            this.amountButton.Name = "amountButton";
+            this.amountButton.Size = new System.Drawing.Size(79, 26);
+            this.amountButton.TabIndex = 17;
+            this.amountButton.Text = "5";
+            // 
+            // betLabel
+            // 
+            this.betLabel.AutoSize = true;
+            this.betLabel.Location = new System.Drawing.Point(315, 538);
+            this.betLabel.Name = "betLabel";
+            this.betLabel.Size = new System.Drawing.Size(177, 20);
+            this.betLabel.TabIndex = 18;
+            this.betLabel.Text = "$ on greyhound number";
+            // 
+            // grehoundNumberButton
+            // 
+            this.grehoundNumberButton.Location = new System.Drawing.Point(505, 536);
+            this.grehoundNumberButton.Name = "grehoundNumberButton";
+            this.grehoundNumberButton.Size = new System.Drawing.Size(50, 26);
+            this.grehoundNumberButton.TabIndex = 19;
+            this.grehoundNumberButton.Text = "1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(839, 579);
+            this.Controls.Add(this.grehoundNumberButton);
+            this.Controls.Add(this.betLabel);
+            this.Controls.Add(this.amountButton);
+            this.Controls.Add(this.betsButton);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.playerTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.playerRadioButton3);
+            this.Controls.Add(this.playerRadioButton2);
+            this.Controls.Add(this.playerRadioButton1);
+            this.Controls.Add(this.minimumBetLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.greyhoundPictureBox4);
+            this.Controls.Add(this.greyhoundPictureBox3);
+            this.Controls.Add(this.greyhoundPictureBox2);
+            this.Controls.Add(this.greyhoundPictureBox1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.raceTrackPictureBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Welcome to The Greyhounds\' Race!";
+            ((System.ComponentModel.ISupportInitialize)(this.raceTrackPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greyhoundPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greyhoundPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greyhoundPictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greyhoundPictureBox4)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox raceTrackPictureBox;
+        private System.Windows.Forms.PictureBox greyhoundPictureBox1;
+        private System.Windows.Forms.PictureBox greyhoundPictureBox2;
+        private System.Windows.Forms.PictureBox greyhoundPictureBox3;
+        private System.Windows.Forms.PictureBox greyhoundPictureBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label minimumBetLabel;
+        private System.Windows.Forms.RadioButton playerRadioButton1;
+        private System.Windows.Forms.RadioButton playerRadioButton2;
+        private System.Windows.Forms.RadioButton playerRadioButton3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox playerTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Button betsButton;
+        private System.Windows.Forms.DomainUpDown amountButton;
+        private System.Windows.Forms.Label betLabel;
+        private System.Windows.Forms.DomainUpDown grehoundNumberButton;
     }
 }
 
