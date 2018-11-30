39e15e53a5e6568d876f9c8f5a385d512714feb9
kpilszak <klaudia.pilszak@gmail.com>
2018-11-30 14:46:33 +0100

    Draft of Greyhound class

--- greyhounds-race/Greyhound.cs
+++ greyhounds-race/Greyhound.cs
@@ -2,5 +2,19 @@
 
 public class Greyhound
 {
-	
+    public int StartingPosition;
+    public int RaceTrackLength;
+    public PictureBox MyPictureBox = null;
+    public int Location = 0;
+    public Random MyRandom;
+
+    public bool Run()
+    {
+
+    }
+
+    public void TakeStartingPosition()
+    {
+
+    }
 }
