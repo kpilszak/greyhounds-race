b3ba3201e31305689748160f3c64bf340d044017
kpilszak <klaudia.pilszak@gmail.com>
2018-12-02 20:27:06 +0100

    Correct Bet.cs

--- greyhounds-race/Bet.cs
+++ greyhounds-race/Bet.cs
@@ -1,6 +1,35 @@
-﻿namespace greyhounds_race
+﻿using System.Windows.Forms;
+
+namespace greyhounds_race
 {
     public class Bet
     {
+        public int Amount;
+        public int Dog;
+        public Player Bettor;
+
+        public string GetDescription()
+        {
+            if (Amount == 0)
+            {
+                return Bettor.Name + " hasn't placed a bet.";
+            }
+            else
+            {
+                return Bettor.Name + " placed a bet of " + Amount + "$ on greyhound number " + Dog;
+            }
+        }
+
+        public int PayOut(int Winner)
+        {
+            if (Dog == Winner)
+            {
+                return Amount;
+            }
+            else
+            {
+                return -Amount;
+            }
+        }
     }
 }
